71ee08f2e2f1c0dd71b66bf1417b583f4459b25e
karan <karannisar811@gmail.com>
2025-09-16 15:07:53 +0530

    base url updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api", // later change to Vercel URL
+  baseURL: "https://saas-notes-app-five.vercel.app/", 
 });
 
 // Add JWT token automatically
